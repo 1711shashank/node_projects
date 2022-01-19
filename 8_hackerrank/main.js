6afce3ca047039195b09d3405739971539b342e2
Kumar Shashank <1711shashank@gmail.com>
2022-01-19 15:13:05 +0530

    optimized code in hackerrank automation

--- 8_hackerrank/main.js
+++ 8_hackerrank/main.js
@@ -38,73 +38,14 @@ browserOpenPromise
     }).then(function(){
         let waitforfewSeconds = page.waitFor(1000);
         return waitforfewSeconds;
+    }).then(function(){
+        let questionArrPromise = page.$$('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled');
+        return questionArrPromise; 
+    }).then(function(questionArr){
+        // console.log(questionArr.length);
+        let questionWillBeSolvedPromise = questionSolver(page, questionArr[0], codeObj.answer[0]);
+        return questionWillBeSolvedPromise;
     })
-    .then(function(){
-        let gotoPagePromise = page.goto("https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true");
-        return gotoPagePromise;
-    }).then(function(){
-        let clickTestInputPromise = waitAndClick("input[type='checkbox']",page);
-        return clickTestInputPromise;
-    }).then(function(){
-        let typeCodePromise = waitAndCode("#input-1",codeObj.answer[0],page);
-        return typeCodePromise;
-    }).then(function(){
-        let waitforfewSeconds = page.waitFor(1000);
-        return waitforfewSeconds;
-    }).then(function(){
-        let ctrlIsPressPromise = page.keyboard.down('Control');
-        return ctrlIsPressPromise;
-    }).then(function(){
-        let AIsPressedPromise = page.keyboard.press('A');
-        return AIsPressedPromise;
-    }).then(function(){
-        let XIsPressedPromise = page.keyboard.press('X',{delay:50});
-        return XIsPressedPromise;
-    }).then(function(){
-        let ctrlIsUnPressPromise = page.keyboard.up('Control');
-        return ctrlIsUnPressPromise;
-    }).then(function(){
-        let mainEditerPromise = page.click('.hr-monaco-editor');
-        return mainEditerPromise;
-    }).then(function(){
-        let ctrlIsPressPromise = page.keyboard.down('Control');
-        return ctrlIsPressPromise;
-    }).then(function(){
-        let AIsPressedPromise = page.keyboard.press('A');
-        return AIsPressedPromise;
-    }).then(function(){
-        let VIsPressedPromise = page.keyboard.press('V',{delay:50});
-        return VIsPressedPromise;
-    }).then(function(){
-        let ctrlIsUnPressPromise = page.keyboard.up('Control');
-        return ctrlIsUnPressPromise;
-    }).then(function(){
-        let waitforfewSeconds = page.waitFor(1000);
-        return waitforfewSeconds;
-    }).then(function(){
-        let clickSubmitPromise = page.click('.hr-monaco-submit');
-        return clickSubmitPromise;
-    }).then(function(){
-        resolve();
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-
-
-
-
-
-    
-    
-    
-    
-    
-
-
-
-
-
 
 function waitAndClick(selector, currpage){
     return new Promise(function(resolve, reject){
@@ -121,48 +62,58 @@ function waitAndClick(selector, currpage){
 
     })
 }
-function waitAndCode(selector, text, currpage){
-    return new Promise(function(resolve, reject){
-        let waitForPageToLoadPromise = page.waitForSelector(selector);
-        waitForPageToLoadPromise
-            .then(function(){
-                let typeOnSelectorPromise = currpage.type(selector,text,{delay:5});
-                return typeOnSelectorPromise;
-            }).then(function(){
-                resolve();
-            }).catch(function(err){
-                reject();
-            })
-    })
     
+     
+function questionSolver(page, question, answer){
+    return new Promise(function(resolve, reject){
+        let questionWillBeClicked = question.click();
+        questionWillBeClicked.then(function(){
+            let editorInFocusPromise = waitAndClick('.monaco-editor.no-user-select.vs',page);
+            return editorInFocusPromise;
+        }).then(function(){
+            return waitAndClick('.checkbox-input', page);
+        }).then(function(){
+            return page.waitForSelector('#input-1', page);
+        }).then(function(){
+            return page.type('#input-1',answer);
+        }).then(function(){
+            let ctrlIsPressPromise = page.keyboard.down('Control');
+            return ctrlIsPressPromise;
+        }).then(function(){
+            let AIsPressedPromise = page.keyboard.press('A',{delay:500});
+            return AIsPressedPromise;
+        }).then(function(){
+            let XIsPressedPromise = page.keyboard.press('X',{delay:500});
+            return XIsPressedPromise;
+        }).then(function(){
+            let ctrlIsUnPressPromise = page.keyboard.up('Control');
+            return ctrlIsUnPressPromise;
+        }).then(function(){
+            let mainEditerPromise = page.click('.hr-monaco-editor');
+            return mainEditerPromise;
+        }).then(function(){
+            let ctrlIsPressPromise = page.keyboard.down('Control');
+            return ctrlIsPressPromise;
+        }).then(function(){
+            let AIsPressedPromise = page.keyboard.press('A',{delay:500});
+            return AIsPressedPromise;
+        }).then(function(){
+            let VIsPressedPromise = page.keyboard.press('V',{delay:500});
+            return VIsPressedPromise;
+        }).then(function(){
+            let ctrlIsUnPressPromise = page.keyboard.up('Control');
+            return ctrlIsUnPressPromise;
+        }).then(function(){
+            let waitforfewSeconds = page.waitFor(1000);
+            return waitforfewSeconds;
+        }).then(function(){
+            let clickSubmitPromise = page.click('.hr-monaco-submit');
+            return clickSubmitPromise;
+        }).then(function(){
+            resolve();
+        }).catch(function(err){
+            console.log(err);
+            reject();
+        })  
+    })
 }
-
-
-
-
-
-
-
-
-
-// function waitAndClick(selector, currpage){
-//     let waitForPageToLoadPromise = page.waitForSelector(selector);
-//     waitForPageToLoadPromise
-//         .then(function(){
-//             let clickOnSelectorPromise = currpage.click(selector);
-//             return clickOnSelectorPromise;
-//         }).catch(function(err){
-//             console.log(err);
-//         })
-// }
-
-// function waitAndType(selector, text, currpage){
-//     let waitForPageToLoadPromise = page.waitForSelector(selector);
-//     waitForPageToLoadPromise
-//         .then(function(){
-//             let typeOnSelectorPromise = currpage.type(selector,text,{delay:100});
-//             return typeOnSelectorPromise;
-//         }).catch(function(err){
-//             console.log(err);
-//         })
-// }
